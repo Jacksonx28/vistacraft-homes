ed6ad9dce8aa9bf3987d27744389a6809ed55695
Jackson <israeljackson699@gmail.com>
2026-04-01 18:59:30 +0100

    feat: add Cloudinary video URLs

---
+++ js/videos.js
@@ -0,0 +1,8 @@
+const VIDEOS = {
+  hero:           "https://res.cloudinary.com/YOUR_CLOUD/video/upload/v.../tea1.mp4",
+  tc2_3d:         "https://res.cloudinary.com/YOUR_CLOUD/video/upload/v.../Teas_Court_2_3D_Completion.mp4",
+  tc1_completion: "https://res.cloudinary.com/YOUR_CLOUD/video/upload/v.../Teas_Court_1_Completion.mp4",
+  kucaro:         "https://res.cloudinary.com/YOUR_CLOUD/video/upload/v.../ProjectKucaro.mp4",
+};
+
+export default VIDEOS;
