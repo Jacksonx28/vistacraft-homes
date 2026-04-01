3d5e0dfc017c1ed0e51b9e5f3f3489a74031c887
Jackson <israeljackson699@gmail.com>
2026-04-01 19:11:49 +0100

    feat: add Cloudinary video URLs

--- js/videos.js
+++ js/videos.js
@@ -1,8 +1,8 @@
 const VIDEOS = {
-  hero:           "https://res.cloudinary.com/YOUR_CLOUD/video/upload/v.../tea1.mp4",
-  tc2_3d:         "https://res.cloudinary.com/YOUR_CLOUD/video/upload/v.../Teas_Court_2_3D_Completion.mp4",
-  tc1_completion: "https://res.cloudinary.com/YOUR_CLOUD/video/upload/v.../Teas_Court_1_Completion.mp4",
-  kucaro:         "https://res.cloudinary.com/YOUR_CLOUD/video/upload/v.../ProjectKucaro.mp4",
+  hero:           "https://res.cloudinary.com/ds13utjxs/video/upload/v1775061254/tea1_icimrm.mp4",
+  tc2_3d:         "https://res.cloudinary.com/ds13utjxs/video/upload/v1775061655/Teas_Court_2_3D_Completion_bzbkuc.mp4",
+  tc1_completion: "https://res.cloudinary.com/ds13utjxs/video/upload/v1775066197/Teas_Court_1_Completion_akcyc4.mp4",
+  kucaro:         "https://res.cloudinary.com/ds13utjxs/video/upload/v1775061396/ProjectKucaro_ylaimk.mp4",
 };
 
 export default VIDEOS;
